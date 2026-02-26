2a9a3bc8901397d0e0278292b6a5380d476a9dc6
Your Name <you@example.com>
2026-02-26 00:12:10 -0300

    Improve tree-sitter config for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,3 +54,16 @@ require('lualine').setup{
     }
 }
 
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'rust', 'c', 'cpp', 'lua', 'python' },
+    callback = function()
+      -- syntax highlighting, provided by Neovim
+      vim.treesitter.start()
+      -- folds, provided by Neovim
+      -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      -- vim.wo.foldmethod = 'expr'
+      -- indentation, provided by nvim-treesitter
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })
+
